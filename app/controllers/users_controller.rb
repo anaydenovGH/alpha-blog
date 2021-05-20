b26961e1d9d7a94678c1378b5041a308b626e07d
Atanas Naydenov <anaydenov93@gmail.com>
2021-05-20 14:38:16 +0200

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,38 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+  
   def new
     @user = User.new
   end
   
+  def edit
+    @user = User.find(params[:id]) # Find the user based on their ID
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Account was edited successfully!"
+      redirect_to @user
+   else
+      render 'edit'
+   end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to Alpha Blog, #{@user.username}"
       redirect_to articles_path
     else
-      flash[:error] = "Something went wrong"
       render 'new'
     end
   end
