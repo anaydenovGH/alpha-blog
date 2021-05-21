4a1b03b5bfb890c4f05e877360d60209f034ebfb
Atanas Naydenov <anaydenov93@gmail.com>
2021-05-21 15:26:23 +0200

    add restrict actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -14,32 +16,50 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id]) # Find the user based on their ID
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account was edited successfully!"
       redirect_to @user
-   else
+    else
       render 'edit'
-   end
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog, #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
     end
   end
-  
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account was successfully deleted"
+    redirect_to root_path
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+  def set_user
+    @user = User.find(params[:id]) # Find the user based on their ID
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You are not allowed to perform this action"
+      redirect_to @user
+    end
+  end
+
 end
